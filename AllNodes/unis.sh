83d93e1ea008685cd5d9ddb6b5056884c02c1c77
treecher <treechcer@gmail.com>
2026-08-07 20:52:22 +0200

    unis added

---
+++ AllNodes/unis.sh
@@ -0,0 +1,10 @@
+#temp script, will be redone later!
+
+#https://askubuntu.com/a/15856
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 
+   exit 1
+fi
+
+sudo userdel Galena
+sudo rm -rd /home/Galena
