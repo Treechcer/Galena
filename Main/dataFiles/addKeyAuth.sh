f385603b71d634bf4641d792e4a16cee3064390d
treecher <treechcer@gmail.com>
2026-08-22 22:56:50 +0200

    updated how ssh config is updated, if it doesn't like have it, it should be apendet (holy who made this word? I have no idea how to write to lol)

--- Main/dataFiles/addKeyAuth.sh
+++ Main/dataFiles/addKeyAuth.sh
@@ -12,3 +12,15 @@ sed 's/PasswordAuthentication yes/PasswordAuthentication no' /etc/ssh/sshd_confi
 sed 's/#PubkeyAuthentication no/PubkeyAuthentication yes' /etc/ssh/sshd_config
 sed 's/PubkeyAuthentication no/PubkeyAuthentication yes' /etc/ssh/sshd_config
 sed 's/#PubkeyAuthentication yes/PubkeyAuthentication yes' /etc/ssh/sshd_config
+
+#TODO: test, this might work?
+
+text=$(cat /etc/ssh/sshd_config)
+
+if ! echo "$text" | grep -q "PubkeyAuthentication yes"; then
+    echo -e "\nPubkeyAuthentication yes" >> /etc/ssh/sshd_config
+fi
+
+if ! echo "$text" | grep -q "PasswordAuthentication no"; then
+    echo -e "\nPasswordAuthentication no" >> /etc/ssh/sshd_config
+fi
