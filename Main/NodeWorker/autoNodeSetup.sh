68df6223e4e79f91a77c17795bd7049aa08fa621
treecher <treechcer@gmail.com>
2026-08-27 16:26:44 +0200

    will sudo help?

--- Main/NodeWorker/autoNodeSetup.sh
+++ Main/NodeWorker/autoNodeSetup.sh
@@ -20,8 +20,8 @@ while [ $i -le 4 ]; do
         #sudo -u Galena ssh $remote "sudo bash -c 'echo \"PasswordAuthentication no\n PubkeyAuthentication yes\"  >> /etc/ssh/sshd_config && sudo systemctl restart ssh'"
 
         sudo -u Galena ssh-copy-id -i ~/.ssh/id_rsa.pub $remote
-        sudo -u Galena ssh $remote bash < ../dataFiles/addKeyAuth.sh
-        sudo -u Galena ssh $remote bash < ../dataFiles/downloadInit.sh
+        sudo -u Galena ssh $remote sudo bash < ../dataFiles/addKeyAuth.sh
+        sudo -u Galena ssh $remote sudo bash < ../dataFiles/downloadInit.sh
 
         #sudo -u Galena ssh $remote touch install.sh
         #sudo -u Galena ssh $remote "echo '#!/bin/bash
--- Main/dataFiles/addKeyAuth.sh
+++ Main/dataFiles/addKeyAuth.sh
@@ -4,22 +4,22 @@ if [ ! -f "/etc/ssh/sshd_config" ]; then
     sudo touch "/etc/ssh/sshd_config"
 fi
 
-sed 's/#PasswordAuthentication no/PasswordAuthentication no/' /etc/ssh/sshd_config
-sed 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sed 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed 's/#PasswordAuthentication no/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 
-sed 's/#PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sed 's/PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sed 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo sed 's/#PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo sed 's/PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo sed 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
 #TODO: test, this might work?
 
 text=$(cat /etc/ssh/sshd_config)
 
 if ! echo "$text" | grep -q "PubkeyAuthentication yes"; then
-    echo -e "\nPubkeyAuthentication yes" >> /etc/ssh/sshd_config
+    sudo echo -e "\nPubkeyAuthentication yes" >> /etc/ssh/sshd_config
 fi
 
 if ! echo "$text" | grep -q "PasswordAuthentication no"; then
-    echo -e "\nPasswordAuthentication no" >> /etc/ssh/sshd_config
+    sudo echo -e "\nPasswordAuthentication no" >> /etc/ssh/sshd_config
 fi
